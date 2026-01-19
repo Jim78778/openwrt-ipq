c9f1628ff330c317c97ca32e0219bcee2ef75b86
L1C <43138541+Jim78778@users.noreply.github.com>
2026-01-19 22:36:58 +0800

    Update ipq807x.mk
    Signed-off-by: L1C <43138541+Jim78778@users.noreply.github.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -1,6 +1,3 @@
-include $(TOPDIR)/rules.mk
-include $(INCLUDE_DIR)/image.mk
-
 define Device/swaiot_cpe_s10
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
@@ -10,22 +7,10 @@ define Device/swaiot_cpe_s10
 	SOC := ipq8071
 
 	DEVICE_DTS := ipq8071-s10
-	# ⚠️ 只有在 DTS 中真实存在时才保留
-	# DEVICE_DTS_CONFIG := config@ac02
-
-	# NAND 镜像参数
 	BLOCKSIZE := 128k
 	PAGESIZE := 2048
 
-	# 镜像类型
-	IMAGES += factory.bin sysupgrade.bin
-	IMAGE/factory.bin := append-ubi | qsdk-ipq-factory-nand
-	IMAGE/sysupgrade.bin := append-ubi
-
-	# 默认安装的软件包
 	DEVICE_PACKAGES := \
-		base-files \
-		busybox \
 		kmod-usb3 \
 		kmod-mhi-bus \
 		kmod-mhi-wwan-mbim \
@@ -34,4 +19,3 @@ define Device/swaiot_cpe_s10
 endef
 
 TARGET_DEVICES += swaiot_cpe_s10
-TARGET_DEVICES := swaiot_cpe_s10
