9e5f04534efd38424afc5ebe97d36505447b395c
L1C <43138541+Jim78778@users.noreply.github.com>
2026-01-19 18:41:41 +0800

    Update ipq807x.mk
    Signed-off-by: L1C <43138541+Jim78778@users.noreply.github.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -13,13 +13,16 @@ define Device/swaiot_cpe_s10
 	# ⚠️ 只有在 DTS 中真实存在时才保留
 	# DEVICE_DTS_CONFIG := config@ac02
 
+	# NAND 镜像参数
 	BLOCKSIZE := 128k
 	PAGESIZE := 2048
 
+	# 镜像类型
 	IMAGES += factory.bin sysupgrade.bin
 	IMAGE/factory.bin := append-ubi | qsdk-ipq-factory-nand
-	IMAGE/sysupgrade.bin := append-ubi | qsdk-ipq-nand
+	IMAGE/sysupgrade.bin := append-ubi
 
+	# 默认安装的软件包
 	DEVICE_PACKAGES := \
 		base-files \
 		busybox \
@@ -31,6 +34,4 @@ define Device/swaiot_cpe_s10
 endef
 
 TARGET_DEVICES += swaiot_cpe_s10
-
-
 TARGET_DEVICES := swaiot_cpe_s10
