c5aa27adc83a2e4370f14d061e52ee8c2c10431a
L1C <43138541+Jim78778@users.noreply.github.com>
2026-01-22 19:22:46 +0800

    Update ipq807x.mk
    Signed-off-by: L1C <43138541+Jim78778@users.noreply.github.com>

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -1,21 +1,35 @@
 define Device/swaiot_cpe_s10
+  # 使用 FIT Image
+  $(call Device/FitImage)
+
+  # NAND + UBI
   $(call Device/UbiFit)
 
   DEVICE_VENDOR := Swaiot
   DEVICE_MODEL := CPE-S10
   DEVICE_VARIANT := NAND
 
-  SOC := ipq8071
+  SOC := ipq807x
   DEVICE_DTS := ipq8071-s10
 
+  # NAND 参数
   BLOCKSIZE := 128k
   PAGESIZE := 2048
 
-  IMAGES := factory.bin sysupgrade.bin
+  # kernel 放进 UBI（关键）
+  KERNEL_IN_UBI := 1
 
-  IMAGE/factory.bin := qsdk-ipq-factory-nand
+  # 明确 kernel 构建方式（关键）
+  KERNEL := kernel-bin | libdeflate-gzip | fit gzip \
+        $(KDIR)/image-$(DEVICE_DTS).dtb
+
+  # 只生成 UBI factory + sysupgrade
+  IMAGES := factory.ubi sysupgrade.bin
+
+  IMAGE/factory.ubi := ubi
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 
+  # 设备默认包
   DEVICE_PACKAGES := \
         kmod-usb3 \
         kmod-mhi-bus \
@@ -23,7 +37,9 @@ define Device/swaiot_cpe_s10
         kmod-mhi-wwan-ctrl \
         kmod-mhi-wwan-mbim \
         luci-proto-qmi \
-        uqmi
+        uqmi \
+        kmod-usb-net-rndis \
+        kmod-usb-net-cdc-ether
 endef
 
 TARGET_DEVICES += swaiot_cpe_s10
